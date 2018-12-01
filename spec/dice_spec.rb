d2dc128e532160fe63dd2b808df52219b7e27435
kiedunne <kie.dunne@gmail.com>
2018-12-01 15:25:05 +0000

    Refactored

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,11 +2,11 @@ require 'dice'
 
  describe Dice do
 
-   it 'Show result of every dice roll' do
+   it 'Displays the result of every dice roll' do
      expect(subject.roll(2).length).to eq 2
    end
 
-   it 'Return only 1-6 randomly when rolled' do
+   it 'Returns a random number from 1-6 when rolled' do
      expect((subject.roll(3)- [1,2,3,4,5,6]).empty?).to eq true
    end
  end
